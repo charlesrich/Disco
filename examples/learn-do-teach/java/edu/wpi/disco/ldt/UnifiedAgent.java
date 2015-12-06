fb3e55902db65c8843f7f1cb93aa1d2a488e2203
Charles Rich <rich@wpi.edu>
2015-12-06 09:30:14 -0500

    learn recipe

--- examples/learn-do-teach/java/edu/wpi/disco/ldt/UnifiedAgent.java
+++ examples/learn-do-teach/java/edu/wpi/disco/ldt/UnifiedAgent.java
@@ -1,10 +1,15 @@
 package edu.wpi.disco.ldt;
 
+import org.w3c.dom.*;
+import javax.xml.parsers.*;
+import edu.wpi.cetask.TaskClass;
 import edu.wpi.disco.*;
 import edu.wpi.disco.Agenda.Plugin;
 import edu.wpi.disco.plugin.*;
 
 public class UnifiedAgent extends Agent {
+   
+   private static Disco disco;
    
    // use this main instead of Interaction.main() for console debugging
    // also see bin/learn-do-teach bash script
@@ -12,7 +17,7 @@ public class UnifiedAgent extends Agent {
       Interaction interaction = new Interaction(new UnifiedAgent("agent"),
             new User("human"), args.length > 0 && args[0].length() > 0 ? args[0]
                : null);
-      Disco disco = interaction.getDisco();
+      disco = interaction.getDisco();
       // tweak glossing
       disco.setProperty("execute@word", "do");
       disco.setProperty("achieve@word", "do");
@@ -36,10 +41,66 @@ public class UnifiedAgent extends Agent {
       return true;
    }
    
-   // convert from JavaScript to ANSI/CEA-2018 and load into Disco
-   public static void compile (String id, String goal, String[] steps) {
-      System.out.println(id);
-      System.out.println(goal);
-      System.out.println(steps[0]+steps[1]);///
+   // code below to convert from JavaScript to ANSI/CEA-2018 and load into Disco
+   // see init script in models/Pedagogical.xml
+   
+   private static DocumentBuilder builder; 
+   
+   { 
+     DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+     factory.setNamespaceAware(true); 
+     try { builder = factory.newDocumentBuilder(); }
+     catch (ParserConfigurationException e) { throw new RuntimeException(e); }
    }
+   
+   private static Document document;
+   private static Element model;
+   private static final String XMLNS = "http://ce.org/cea-2018";
+   
+   public static void compileTasks (String[] tasks) {
+      document = builder.newDocument();
+      model = document.createElementNS(XMLNS, "taskModel");
+      document.appendChild(model);
+      // boiler plate
+      Attr about = document.createAttribute("about");
+      about.setValue("urn:disco.wpi.edu:learn-do-teach");
+      model.setAttributeNode(about);
+      Attr xmlns = document.createAttribute("xmlns");
+      xmlns.setValue(XMLNS);
+      model.setAttributeNode(xmlns);
+      Attr primitive = document.createAttributeNS("http://www.w3.org/2000/xmlns/", "xmlns:primitive");
+      primitive.setValue("urn:disco.wpi.edu:ldt:Primitives");
+      model.setAttributeNode(primitive);
+      for (int i = 0; i < tasks.length; i++) {
+         Element task = document.createElementNS(XMLNS, "task");
+         model.appendChild(task);
+         Attr id = document.createAttribute("id");
+         id.setValue(tasks[i]);
+         task.setAttributeNode(id);
+      }
+   }
+   
+   public static void compileRecipe (String id, String goal, String[] steps) {
+      Element subtasks = document.createElementNS(XMLNS, "subtasks");
+      model.appendChild(subtasks);
+      Attr idAttr = document.createAttribute("id");
+      idAttr.setValue(id);
+      subtasks.setAttributeNode(idAttr);
+      Attr goalAttr = document.createAttribute("goal");
+      goalAttr.setValue(goal);
+      subtasks.setAttributeNode(goalAttr);
+      for (int i = 0; i < steps.length; i++) {
+         Element step =  document.createElementNS(XMLNS, "step");
+         subtasks.appendChild(step);
+         Attr name = document.createAttribute("name");
+         name.setValue("step"+(i+1));
+         step.setAttributeNode(name);
+         Attr taskAttr = document.createAttribute("task");
+         TaskClass task = disco.getTaskClass(steps[i]);
+         taskAttr.setValue(((task != null && task.isPrimitive()) ? "primitive:" : "")+steps[i]);
+         step.setAttributeNode(taskAttr);
+      }
+   }  
+   
+   public static void load () { disco.load(document, null); }
 }
