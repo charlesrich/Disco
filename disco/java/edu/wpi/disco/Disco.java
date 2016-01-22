cc0683faba02cfe7cd8025d396b809582afde302
Charles Rich <rich@wpi.edu>
2016-01-21 19:11:31 -0500

    Release 1.14

--- disco/java/edu/wpi/disco/Disco.java
+++ disco/java/edu/wpi/disco/Disco.java
@@ -40,7 +40,7 @@ public class Disco extends TaskEngine {
          .start(true); // prompt user first
    }
    
-   public static String VERSION = "1.13";
+   public static String VERSION = "1.14";
    
    /**
     * To enabled tracing of Disco implementation.  Note this variable can be conveniently
